61e9ceda7192b14dea0bcc32db5c82adb7d0266f
Don Mallory <dmm333@gmail.com>
2020-02-22 00:54:56 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -68,7 +68,7 @@ def reduce_to_any_true(source_array)
   value = false
   while index < source_array.length
     value = value || source_array[index]
-    puts value
+    puts index + ":    " + value
     index += 1
   end
   value
